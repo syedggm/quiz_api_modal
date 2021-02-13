cb79f8c6c36b328483639eb18032ba748f081dc2
syedggm <syedggm@gmail.com>
2021-02-13 20:44:34 +0530

    Fully functional API based quiz app

---
+++ src/SetupForm.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useGlobalContext } from './context'
+
+const SetupForm = () => {
+  const {handleSubmit, handleChange, quiz, error} = useGlobalContext()
+  return <main>
+    <section className="quiz quiz-small">
+    <h2>setup quiz</h2>
+      <form className="setup-form">
+        <div className="form-control">
+          <label htmlFor="amount">number of questions</label>
+          <input type="number" name="amount" id="amount"
+          value={quiz.amount} onChange={handleChange}
+          className="form-input" min={1} max={50}
+          />
+        </div>
+        <div className="form-control">
+          {/* category */}
+          <label htmlFor="category">category</label>
+          <select name="category" id="category" className="form-input" value={quiz.category} onChange={handleChange}>
+            <option value="sports">sports</option>
+            <option value="history">history</option>
+            <option value="computers">computers</option>
+          </select>
+        </div>
+        <div className="form-control">
+          {/* difficulty */}
+          <label htmlFor="difficulty">difficulty</label>
+          <select name="difficulty" id="difficulty" className="form-input" value={quiz.difficulty} onChange={handleChange}>
+            <option value="easy">easy</option>
+            <option value="medium">medium</option>
+            <option value="hard">hard</option>
+          </select>
+        </div>
+        {error && <p className="error">can't generate questions, please different options</p>}
+        <button type="submit" onClick={handleSubmit} className="submit-btn">start</button>
+      </form>
+    </section>
+    </main>
+}
+
+export default SetupForm
